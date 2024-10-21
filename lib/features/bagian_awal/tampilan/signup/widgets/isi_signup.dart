d097ea4b61dcf7a16b981e83d9b1f9c50b4ddb69
MarvelFanuel <c14220113@john.petra.ac.id>
2024-10-21 22:50:46 +0700

    Update isi_signup.dart
    hapus alamat no telp tanggal lahir resub pass

--- lib/features/bagian_awal/tampilan/signup/widgets/isi_signup.dart
+++ lib/features/bagian_awal/tampilan/signup/widgets/isi_signup.dart
@@ -59,15 +59,6 @@ class IsiSignup extends StatelessWidget {
                   ),
                   TextFormField(
                     decoration: const InputDecoration(
-                      labelText: 'Alamat',
-                      labelStyle: TextStyle(
-                          fontFamily: 'NunitoSans',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.0),
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
                       labelText: 'Email',
                       labelStyle: TextStyle(
                           fontFamily: 'NunitoSans',
@@ -77,34 +68,7 @@ class IsiSignup extends StatelessWidget {
                   ),
                   TextFormField(
                     decoration: const InputDecoration(
-                      labelText: 'No. Telpon',
-                      labelStyle: TextStyle(
-                          fontFamily: 'NunitoSans',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.0),
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Tanggal Lahir',
-                      labelStyle: TextStyle(
-                          fontFamily: 'NunitoSans',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.0),
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
                       labelText: 'Password',
-                      labelStyle: TextStyle(
-                          fontFamily: 'NunitoSans',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14.0),
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Re-confirm Password',
                       labelStyle: TextStyle(
                           fontFamily: 'NunitoSans',
                           fontWeight: FontWeight.w500,
