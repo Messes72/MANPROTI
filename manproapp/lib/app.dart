5568b99f36848f8d23b6e2910a74fd0a83696858
Timothy VC <c14220197@john.petra.ac.id>
2024-11-28 10:30:25 +0700

    test

---
+++ manproapp/lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:manpro/features/bagian_awal/tampilan/login/login.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: Login(),
+    );
+  }
+}
---
+++ manproapp/lib/utils/constants/api_constants.dart
@@ -0,0 +1,6 @@
+// //List of API
+// class APIContants {
+//   // static const
+// }
+
+String url = "http://192.168.56.1:80/api/";
