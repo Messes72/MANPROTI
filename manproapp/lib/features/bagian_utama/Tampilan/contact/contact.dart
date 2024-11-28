904eedeebb6a8680db7bd6ba456b0c2f9df1f1fd
Timothy VC <c14220197@john.petra.ac.id>
2024-11-28 19:52:47 +0700

    contact

--- manproapp/lib/features/bagian_utama/Tampilan/contact/contact.dart
+++ manproapp/lib/features/bagian_utama/Tampilan/contact/contact.dart
@@ -3,34 +3,47 @@ import 'package:manpro/common/widgets/background_app.dart';
 import 'package:manpro/features/bagian_utama/Tampilan/contact/contact_yayasan_content.dart';
 import 'package:manpro/features/bagian_utama/Tampilan/contact/tulisan_contact.dart';
 import 'package:manpro/features/bagian_utama/Tampilan/profile_yayasan/widgets/tombol_back.dart';
+import 'package:get/get.dart';
+import 'package:manpro/navbar.dart';
+import 'package:manpro/utils/constants/image_string.dart';
 
 class KontakYayasan extends StatelessWidget {
   const KontakYayasan({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Stack(
         children: [
-          BackgroundAPP(),
+          const BackgroundAPP(),
           SafeArea(
             child: SingleChildScrollView(
               child: Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     // Tombol back
-                    TombolBack(),
+                    IconButton(
+                      onPressed: () {
+                        final controller = Get.put(NavigationController());
+                        controller.selectedIndex.value = 0; // Switch to Home tab
+                        Get.to(() => const Navbar());
+                      },
+                      icon: const ImageIcon(
+                        AssetImage(YPKImages.icon_back_button),
+                        size: 32.0,
+                      ),
+                    ),
 
                     // Tulisan Kontak Yayasan
-                    TulisanKontak(),
-                    SizedBox(
+                    const TulisanKontak(),
+                    const SizedBox(
                       height: 35.0,
                     ),
 
                     // Kontak Yayasan
-                    Padding(
+                    const Padding(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: KontakYayasanContent(),
                     ),
